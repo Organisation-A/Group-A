b4a04dc09ffb1f7aa09692b71d6c7b15004dd9d3
wendymaboa <2541693@students.wits.ac.za>
2024-09-05 10:06:01 +0200

    Homepage(done)

--- src/Components/SideMenu/SideMenu.jsx
+++ src/Components/SideMenu/SideMenu.jsx
@@ -3,24 +3,30 @@ import { FiMapPin } from "react-icons/fi";  // For FiMapPin
 import { GrBus } from "react-icons/gr";    // For GrBus
 import { CiRoute, CiLogout } from "react-icons/ci";  // For CiRoute and CiLogout
 import './SideMenu.css'; // Import your CSS file
+import logo from '../Assets/black_logo.jpg';
 
 const SideMenu = () => {
   return (
     <div className="side-menu">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="menu-divider"></div> {/* Divider Line */}
+  
       <div className="menu-item">
-      <FiMapPin className='icon'/>
+        <span> <FiMapPin className="icon" /></span>
         <span className="menu-text">Home</span>
       </div>
       <div className="menu-item">
-        <GrBus className='icon'/>
+        <span><GrBus className="icon" /></span>
         <span className="menu-text">Bus Schedule</span>
       </div>
       <div className="menu-item">
-        <CiRoute className='icon'/>
+        
+        <span><CiRoute className="icon" /></span>
         <span className="menu-text">Rentals</span>
       </div>
       <div className="menu-item">
-        <CiLogout className='icon'/>
+        
+        <span><CiLogout className="icon" /></span>
         <span className="menu-text">Logout</span>
       </div>
     </div>
