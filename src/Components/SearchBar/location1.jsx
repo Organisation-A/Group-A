ccefd3d895c32455853c5c84f2215b8af022b264
wendymaboa <2541693@students.wits.ac.za>
2024-09-17 09:34:33 +0000

    updates

---
+++ src/Components/SearchBar/location1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LabPage = () => {
+  return (
+    <div>
+      <h1>Mathematical Science Laboratories</h1>
+      <p>Details about the Mathematical Science Laboratories...</p>
+    </div>
+  );
+};
+
+export default LabPage;
