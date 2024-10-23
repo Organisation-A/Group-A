032d3e3d5b6f18f74b417c3351f4bd6dda08bcf0
souoGael <160495329+souoGael@users.noreply.github.com>
2024-10-23 23:19:08 +0200

    Update BuildingMap.jsx

--- src/Components/Map/BuildingMap.jsx
+++ src/Components/Map/BuildingMap.jsx
@@ -453,7 +453,7 @@ const BuildingMap = () => {
   }, [googleMaps, userData.location, events]);
   useEffect(() => {
     const loader = new Loader({
-      apiKey: "AIzaSyAGaYeN_3hgrGi1h_aJRIB4MAvipQKVToA",
+      apiKey: process.env.MAP_KEY,
       version: "weekly",
       libraries: ["places"],
     });
